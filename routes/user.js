e0f7ea86db93ec84b6223be70eab02f13090ec1a
Govinda Lovanshi <134533021+GovindaLovanshi@users.noreply.github.com>
2023-12-02 13:47:48 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const wrapAsnc = require("../utils/wrapAsnc");
+const wrapAsnc = require("../utils/wrapAsnc.js");
 const passport = require("passport");
 const { reviewsSchema } = require("../schema");
 const { saveRedirectUrl } = require("../middleware");
